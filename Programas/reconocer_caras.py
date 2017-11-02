5c5b7278466a221a83f07b75ca0d04762ab46dd8
domingo gomez <domingo.gomez@unican.es>
2017-11-02 10:53:37 +0100

    Versión corregida del programa reconocer_caras.py
    Problemas detectados, no se cierra correctamente la ventana.

--- Programas/reconocer_caras.py
+++ Programas/reconocer_caras.py
@@ -4,8 +4,12 @@ import os.path
 import numpy as np
 import cv2
 
+# THIS DIRECTORY HAS TO BE MANUALLY CONFIGURED!
+DIRECTORY = os.path.join('/','home','domingo','anaconda3','envs','test','share','OpenCV','haarcascades')
+FILENAME = 'haarcascade_frontalface_default.xml'
+CLASSIFIER = os.path.join(DIRECTORY,FILENAME)
 cap = cv2.VideoCapture(0)
-
+print(CLASSIFIER)
 while(True):
     # Capture frame-by-frame
     ret, frame = cap.read()
@@ -13,9 +17,9 @@ while(True):
     # Our operations on the frame come here
     gray = cv2.cvtColor(frame, cv2.COLOR_BGR2GRAY)
     # We define the classifier (these files have to be found)
-    face_cascade = cv2.CascadeClassifier('haarcascade_frontalface_default.xml')
+    face_cascade = cv2.CascadeClassifier(CLASSIFIER)
     # Here we detect the faces 
-    faces = face_cascade.detectMultiScale(gray, 1.3, 5)
+    faces = face_cascade.detectMultiScale(frame, 1.3, 5)
     for (x,y,w,h) in faces:
         cv2.rectangle(frame,(x,y),(x+w,y+h),(255,0,0),2)
     # Display the resulting frame
